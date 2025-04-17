e45ba9b9a15134b4bd3fe2ac3fef4b9ba1e30dda
Amin Alam <ma.alamalhoda@gmail.com>
2025-04-17 15:26:40 +0200

    dashboard re-design

--- server/src/main.py
+++ server/src/main.py
@@ -19,6 +19,26 @@ class PlantData(db.Model):
     def __repr__(self):
         return f"<PlantData {self.plant_name}>"
 
+# Function to determine moisture status
+def get_moisture_status(moisture_value):
+    if moisture_value > 3000:
+        return {"class": "dry", "text": "Dry"}
+    elif moisture_value > 2400:
+        return {"class": "not-so-wet", "text": "Not So Wet"}
+    elif moisture_value > 2100:
+        return {"class": "wet", "text": "Wet"}
+    else:  # moisture_value <= 2100
+        return {"class": "very-wet", "text": "Very Wet"}
+
+# Function to determine threshold values for status calculations
+def get_moisture_thresholds():
+    return {
+        "very_wet": 2100,
+        "wet": 2400,
+        "not_so_wet": 3000,
+        "dry": float('inf')
+    }
+
 # Route to receive data from Arduino
 @app.route('/receive_data', methods=['POST'])
 def receive_data():
@@ -45,7 +65,34 @@ def receive_data():
 # Route to display data
 @app.route('/')
 def index():
-    plant_data = PlantData.query.order_by(PlantData.timestamp.desc()).all()
+    # Check if time_range parameter is provided
+    time_range = request.args.get('time_range', 'all')
+    
+    # By default, get all data ordered by timestamp
+    query = PlantData.query
+    
+    # If time_range is specified and not 'all', filter by date
+    if time_range != 'all' and time_range.isdigit():
+        days = int(time_range)
+        cutoff_date = datetime.datetime.utcnow() - datetime.timedelta(days=days)
+        query = query.filter(PlantData.timestamp >= cutoff_date)
+    
+    # Get data ordered by timestamp
+    plant_data_raw = query.order_by(PlantData.timestamp.desc()).all()
+    
+    # Add moisture status to each data point
+    plant_data = []
+    for data in plant_data_raw:
+        data_dict = {
+            'id': data.id,
+            'plant_name': data.plant_name,
+            'location': data.location,
+            'moisture_value': data.moisture_value,
+            'timestamp': data.timestamp,
+            'moisture_status': get_moisture_status(data.moisture_value)
+        }
+        plant_data.append(data_dict)
+    
     return render_template('index.html', plant_data=plant_data)
 
 if __name__ == '__main__':
@@ -58,6 +105,6 @@ if __name__ == '__main__':
 
 def run():
     """Entry point for the application script"""
-    with app.app_context():
-        db.create_all()
+    # with app.app_context():
+    #     db.create_all()
     app.run(host='0.0.0.0', port=8080)
